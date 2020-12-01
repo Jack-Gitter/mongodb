e8397c2ab15af70767c18664cdb7ab0164c14d18
Jack Gitter <gitter.j@husky.neu.edu>
2020-12-01 14:34:35 -0500

    fifth commit

--- daos/questions-dao.js
+++ daos/questions-dao.js
@@ -1,4 +1,4 @@
-const questionsModel = require('../models/questions/questions-model-model.js')
+const questionsModel = require('../models/questions/questions-model')
 const findAllQuestions = () => questionsModel.find()
 const findQuestionById = (quizId) => questionsModel.findById(quizId)
 const findQuestionForQuiz = (qzid) => questionsModel.find({quizId: qzid})
